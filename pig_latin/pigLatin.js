7ae12164b9f7c55499b7dc2e2e8bbe836549b6b6
CurmudJim <jgarvey86@gmail.com>
2018-02-07 12:13:21 -0600

    Completed pig latin exercise

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,18 @@
-function translate() {
-	// body...
+function translate(string) {
+	let stringArray = string.split(" ");
+  let newArray = [];
+	stringArray.forEach(function(e) {
+	  let f = "";
+	  while (e[0].match(/[aeiouy]/ig) === null || (f.includes("q") && e[0] === "u")) {
+	    f = f + e[0];
+	    e = e.substr(1);
+	  }
+	  newArray.push(e + f + "ay");
+  });
+  return newArray.join(" ");
 }
 
 
 module.exports = {
 	translate
 }
-
